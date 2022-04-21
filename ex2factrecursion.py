ae2cf340f754f674c36175133bd8ddee01cc1584
YATHAM <yatham024@gmail.com>
2022-04-21 23:24:56 +0530

    Add files via upload

---
+++ ex2factrecursion.py
@@ -0,0 +1,13 @@
+def fact(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * fact(n - 1)
+
+
+n = int(input("Enter the number:"))
+if n < 0:
+    print("Invalid input")
+else:
+    f = fact(n)
+    print("Factorial of {} is: {}".format(n, f))
