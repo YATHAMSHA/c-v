b5d94e089d6574d1052ae95077c1a4a53777feb3
YATHAM <yatham024@gmail.com>
2022-04-22 23:30:32 +0530

    Add files via upload

---
+++ ex4adeclare.py
@@ -0,0 +1,13 @@
+string = "YATHAM"
+print(string)
+
+string = "YATHAM"
+print(string)
+
+string = """YATHAM"""
+print(string)
+
+
+string = """YATHAM, welcome to
+           the world of Python"""
+print(string)
