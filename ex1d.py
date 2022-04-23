7397676a1a7ca7609fdd0e1f057e1798626ea4e7
YATHAM <yatham024@gmail.com>
2022-04-23 23:55:27 +0530

    Add files via upload

---
+++ ex1d.py
@@ -0,0 +1,12 @@
+a = 20
+b = 10
+c = 10
+d = 5
+e = (a + b) * c / d
+print("Value of (a + b) * c / d = ", e)
+e = ((a + b) * c) / d
+print("Value of ((a + b) *c) / d = ", e)
+e = (a + b) * (c / d)
+print("Value of (a + b) * (c / d) = ", e)
+e = a + (b * c) / d
+print("Value of a + (b * c) / d = ", e)
