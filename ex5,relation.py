8a6a813bb19de5ed6e546b12ecab442dcab9cb42
YATHAM <yatham024@gmail.com>
2022-04-26 23:46:31 +0530

    Add files via upload

---
+++ ex5,relation.py
@@ -0,0 +1,20 @@
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+
+great = a > b
+print("{0} > {1} is {2}".format(a, b, great))
+
+less = a < b
+print("{0} < {1} is {2}".format(a, b, less))
+
+eql = a == b
+print("{0} == {1} is {2}".format(a, b, eql))
+
+neql = a != b
+print("{0} != {1} is {2}".format(a, b, neql))
+
+greql = a >= b
+print("{0} >= {1} is {2}".format(a, b, greql))
+
+leseql = a <= b
+print("{0} <= {1} is {2}".format(a, b, leseql))
