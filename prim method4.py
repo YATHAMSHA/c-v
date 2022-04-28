8493f4856f183c02d3cd1c5fa9cb63942ceb1d59
YATHAM <yatham024@gmail.com>
2022-04-28 21:22:00 +0530

    Add files via upload

---
+++ prim method4.py
@@ -0,0 +1,11 @@
+lower = int(input("Enter the lower bound: "))
+upper = int(input("Enter the upper bound: "))
+
+print("Prime numbers between", lower, "and", upper, "are:")
+for num in range(lower, upper + 1):
+    if num > 1:
+        for i in range(2, (num // 2) + 1):
+            if num % i == 0:
+                break
+        else:
+            print(num, end=",")
