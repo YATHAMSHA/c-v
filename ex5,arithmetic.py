580efdecd1a7b474ddf068430f88a5fcf79cb82c
YATHAM <yatham024@gmail.com>
2022-05-02 22:44:47 +0530

    Add files via upload

---
+++ ex5,arithmetic.py
@@ -0,0 +1,16 @@
+a = int(input("Enter first number "))
+b = int(input("Enter second number "))
+sum = a + b
+sub = a - b
+mul = a * b
+div = a / b
+mod = a % b
+exp = a ** b
+flr = a // b
+print(f"sum of {a} and {b} = {sum}")
+print(f"substarction of {a} and {b} = {sub}")
+print(f"multiplication of {a} and {b} = {mul}")
+print(f"div of {a} and {b} = {div}")
+print(f"mod of {a} and {b} = {mod}")
+print(f"exponention of {a} and {b} = {exp}")
+print(f"floor div of {a} and {b} = {flr}")
