3d37853231c200d633221084b656450c34f9770d
YATHAM <yatham024@gmail.com>
2022-05-06 21:07:24 +0530

    Add files via upload

---
+++ ex1c.py
@@ -0,0 +1,16 @@
+a = int(input("Enter first number "))
+b = int(input("Enter second number "))
+sum = a + b
+diff = a - b
+prod = a * b
+div = a / b
+rem = a % b
+pow = a ** b
+flr = a // b
+print(f"sum of {a} and {b} = {sum}")
+print(f"diff of {a} and {b} = {diff}")
+print(f"prod of {a} and {b} = {prod}")
+print(f"div of {a} and {b} = {div}")
+print(f"rem of {a} and {b} = {rem}")
+print(f"exponention of {a} and {b} = {pow}")
+print(f"floor div of {a} and {b} = {flr}")
