66865da2314dbe7aecaeee1f5b7ac7398db87181
YATHAM <yatham024@gmail.com>
2022-05-07 23:13:26 +0530

    Add files via upload

---
+++ ex1b.py
@@ -0,0 +1,17 @@
+a = int(input("Enter the first numb:"))
+b = int(input("Enter the second numb:"))
+print("before swapping a=", a, "and b=", b)
+a, b = b, a
+print("after swapping a=", a, "and b=", b)
+
+a = float(input("Enter the first numb:"))
+b = float(input("Enter the second numb:"))
+print("before swapping a=", a, "and b=", b)
+a, b = b, a
+print("after swapping a=", a, "and b=", b)
+
+a = input("Enter the first string:")
+b = input("Enter the second string:")
+print("before swapping a=", a, "and b=", b)
+a, b = b, a
+print("after swapping a=", a, "and b=", b)
