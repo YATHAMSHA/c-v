cb7005839df05af147b543f2425094540a7a1080
YATHAM <yatham024@gmail.com>
2022-05-09 23:04:37 +0530

    Add files via upload

---
+++ ex5,assignment.py
@@ -0,0 +1,28 @@
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+c = a
+
+print("a = {0}".format(a))
+
+a += b
+print("a += b = {0}".format(a))
+a = c
+
+a -= b
+print("a -= b = {0}".format(a))
+a = c
+
+a *= b
+print("a *= b = {0}".format(a))
+a = c
+
+a %= b
+print("a %= b = {0}".format(a))
+a = c
+
+a **= b
+print("a **= b = {0}".format(a))
+a = c
+
+a //= b
+print("a //= b = {0}".format(a))
