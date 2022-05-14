41816f0de7bda7ae533e9166d028d59030bdba72
YATHAM <yatham024@gmail.com>
2022-05-14 23:44:15 +0530

    Add files via upload

---
+++ ex2faciteration.py
@@ -0,0 +1,18 @@
+def fact(n):
+    f = 1
+    i = 1
+    while i <= n:
+        f = f * i
+        i = i + 1
+    return f
+
+
+n = int(input("Enter the number:"))
+if n < 0:
+    print("Invalid input")
+elif n == 0:
+    print("Factorial of 0 is 1")
+
+else:
+    f = fact(n)
+    print("Factorial of {} is : {}".format(n, f))
