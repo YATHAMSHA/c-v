69d8c30a2cf67048e3621205bbd5a325f183bbea
YATHAM <yatham024@gmail.com>
2022-05-23 22:21:59 +0530

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,39 @@
+a = 12
+b = 6.0
+d = True
+c = "Python is good language"
+e = [4, 5.0, "5 sem", "yatham"]
+f = {
+    4,
+    5,
+    6,
+    7,
+    8,
+    11,
+}
+g = (4, 5.0, "Python", "yatham")
+h = {"a": 4.5, "Python": "Language"}
+
+print("value of a=", a)
+print("Type of a=", type(a))
+
+print("value of b=", b)
+print("Type of b=", type(b))
+
+print("value of c=", c)
+print("Type of c=", type(c))
+
+print("value of d=", d)
+print("Type of d=", type(d))
+
+print("value of e=", e)
+print("Type of e=", type(e))
+
+print("value of f=", f)
+print("Type of f=", type(f))
+
+print("value of g=", g)
+print("Type of g=", type(g))
+
+print("value of h=", h)
+print("Type of h=", type(h))
