455817b5135209c468a62da53d09f2101f2ed9ab
YATHAM <yatham024@gmail.com>
2022-05-27 23:35:21 +0530

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,14 @@
+def sum_and_avg_of_natural_numbers(num):
+    if num == 0:
+        return num
+    else:
+        return num + sum_and_avg_of_natural_numbers(num - 1)
+
+
+number = int(input("Please Enter any Number: "))
+
+total = sum_and_avg_of_natural_numbers(number)
+average = total / number
+
+print("The Sum of first {0} Natural Numbers  =  {1}".format(number, total))
+print("Average of first {0} Natural Numbers  =  {1}".format(number, average))
